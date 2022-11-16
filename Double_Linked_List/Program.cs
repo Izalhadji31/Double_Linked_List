cfadf0e15093821aac608280057febaea65905aa
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-11-16 16:33:36 +0700

    Menambahkan class DoubleLinkedList

--- Double_Linked_List/Program.cs
+++ Double_Linked_List/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace Double_Linked_List
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
-        {
-        }
+        /*Node class represents the node of doubly linked list.
+         * It consists of the information part and links to
+         * itss succeeding and preceeding nodes
+         * in terms of next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the succeeding node*/
+        public Node prev;/*points to the preceeding node*/
+    }
+    class DoubleLinkedList
+    {
+
     }
 }
