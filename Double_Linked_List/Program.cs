396deeb687a084e3bfd016edcf5074c2ba718608
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-11-16 17:07:56 +0700

    Menambahakan Deletes the specified node

--- Double_Linked_List/Program.cs
+++ Double_Linked_List/Program.cs
@@ -19,6 +19,77 @@ namespace Double_Linked_List
     }
     class DoubleLinkedList
     {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNote()/*Adds a new node*/
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            /*Checks if the list is empty*/
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                newnode.next = START;
+                if (START != null)
+                    START.prev = newnode;
+                newnode.prev = null;
+                START = newnode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = START; current != null &&
+                rollNo >= current.rollNumber; previous = current, current =
+                current.next)
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return ;
+                }
+            }
+            /*On the execution of the above for loop, prev and
+             * current will pont to those nodes
+            between which the new node is to be inserted.*/
+            newnode.next = current;
+            newnode.prev = previous;
 
+            /*If  the node is to be inserted at the end of the list.*/
+            if (current == null)
+            {
+                newnode.next = null;
+                previous.next = newnode;
+                return ;
+            }
+            current.prev = newnode;
+            previous.next = newnode;
+        }
+
+        /*Checks wheteher the specified node is present*/
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for(previous = current = START; current != null &&
+                rollNo != current.rollNumber; previous = current,
+                current = current.next)
+            { }
+            /*The above for loop traverses the list. If the specified node
+             * is found then the function returns true, otherwise false.*/
+            return (current != null);
+        }
+
+        public bool delNode(int rollNo)/*Deletes the specified node
     }
 }
